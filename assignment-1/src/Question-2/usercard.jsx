8b279aa8002fcea2ff0c6e45e413f21a1d70680f
Vaibhav-successiveTech <vaibhav.sharma@successive.tech>
2025-06-18 11:17:50 +0530

    assignment-1

--- assignment-1/src/Question-2/usercard.jsx
+++ assignment-1/src/Question-2/usercard.jsx
@@ -1,7 +1,8 @@
 function userCard(props){
     return (
         <div>
-            <p>{props.name}  {props.email} <img src = {props.image}/> </p>
+            <p>{props.name}  {props.email} <img src = {props.image} style={{width : '50px',height:'50px'}}/>
+            </p> 
         </div>
     )
 }
--- assignment-1/src/app/page.js
+++ assignment-1/src/app/page.js
@@ -5,7 +5,6 @@ import ChangeCount from "@/Question-4/count";
 import TaskList from "@/Question-5/taskList";
 import Button from "@/Question-6/button";
 
-
 // image1 = 'https://lnk.ink/wlXSO'
 // image2 = 'https://lnk.ink/pGKId'
 // image3 = 'https://lnk.ink/7vkiI'
@@ -14,7 +13,12 @@ import Button from "@/Question-6/button";
 export default function Home() {
    let list = ['task1','task2','task3','task4','task5'];
   return (
-     <Button text = 'This is Button' color='Blue'></Button>
+     //<Button text = 'This is Button' color='Blue'></Button>
     // <TaskList list={list}/>
+    <>
+    <UserCard name = 'Vaibhav' email = 'vaibhav@successive.tech' image = './man.png'/>
+    <UserCard name = 'Aklhi' email = 'aklhil@successive.tech' image = './man.png'/>
+    <UserCard name = 'Divyanshu' email = 'Divyanshu@successive.tech' image = './man.png'/>
+    </>
   );
 }

fe7eafa979fb6f0d78205428feb06919271d9124
Vaibhav-successiveTech <vaibhav.sharma@successive.tech>
2025-06-24 18:03:56 +0530

    assignment-2

--- assignments/src/Assignment2Components/timer/TimerHook.jsx
+++ assignments/src/Assignment2Components/timer/TimerHook.jsx
@@ -5,12 +5,15 @@ const useTimer = () => {
     let [interval,setInt] = useState(null);
     
     const start = (count,setCount)=>{
-            if(count > 0){
-               let i = setInterval(()=>{
-                    setCount(prev=>prev-1);
-                },1000);
-                setInt(i);
-            }
+        if(count>0){
+            let i = setInterval(()=>{
+                setCount((prev)=>{
+                    if(prev==0)return 0;
+                    return prev-1;
+                });
+            },1000);
+            setInt(i);
+        }
     }
 
     const pause = ()=>{

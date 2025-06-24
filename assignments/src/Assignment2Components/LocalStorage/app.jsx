80a4bf823cb3d97885bf8e91c3f02d6e1bc19ee6
Vaibhav-successiveTech <vaibhav.sharma@successive.tech>
2025-06-24 17:39:08 +0530

    assignment-2

--- assignments/src/Assignment2Components/LocalStorage/app.jsx
+++ assignments/src/Assignment2Components/LocalStorage/app.jsx
@@ -10,7 +10,7 @@ export default function LocalStorageApp(){
     return (
         <>
             <input type='text' value={key} onChange={(e)=>setKey(e.target.value)}/><label> key </label>
-            <input id="1" type='text' value={val} onChange={(e)=>setVal(e.target.value)}/><label> val </label>
+            <input type='text' value={val} onChange={(e)=>setVal(e.target.value)}/><label> val </label>
             <br/>
             <button onClick={()=>{
                 setter(key,val);
@@ -19,7 +19,6 @@ export default function LocalStorageApp(){
             }}> Add to Local Storage </button> <button onClick={()=>{
                 remove(key);
             }}> Remove From LocalStorage </button> <button onClick={()=>{
-                document.getElementById('1').disable = true;
                 setVal(getter(key));
             }}> Get From Local Storage </button>
         </>

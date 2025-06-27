29890e19e09f652222a66ddee45e2ccd89ea887e
Vaibhav Sharma <vaibhav.sharma@vaibhavvrs-08.successivetech.com>
2025-06-27 10:13:23 +0530

    added Go Back Button

--- assignments/src/Assignment3Comopnents/products/list.jsx
+++ assignments/src/Assignment3Comopnents/products/list.jsx
@@ -1,3 +1,4 @@
+'use client';
 import Link from "next/link";
 import { ProductList } from "./product";
 export default function ListComponent(){
--- assignments/src/Assignment3Comopnents/products/product.jsx
+++ assignments/src/Assignment3Comopnents/products/product.jsx
@@ -1,4 +1,4 @@
-
+'use client'
 let ProductList = [
         {id : 1 , name : "Product 1", desc : "Product 1 description"},
         {id : 2 , name : "Product 2", desc : "Product 2 description"},
@@ -6,13 +6,19 @@ let ProductList = [
         {id : 4 , name : "Product 4", desc : "Product 4 description"},
         {id : 5 , name : "Product 5", desc : "Product 5 description"},
     ]
-
+import { useRouter } from "next/navigation";
 export default function ProductComponent({id}){
     const obj = ProductList[id-1];
+    const router = useRouter();
     return (
        <>
        <h3> Product : {obj.id} </h3>
        <p> Description : {obj.desc} </p>
+       <br/>
+       <br/>
+       <button onClick={()=>{
+            router.push('/Assignment-3/Question6')
+       }}> Go Back </button>
        </>
     );
 }
